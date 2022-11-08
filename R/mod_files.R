6fc2dfb4ad9d6ac13338266278407fad6363383f
Rico Derks <r.j.e.derks@lumc.nl>
2022-11-08 11:37:02 +0100

    added little r to doc.

--- R/mod_files.R
+++ R/mod_files.R
@@ -2,7 +2,7 @@
 #'
 #' @description A shiny Module.
 #'
-#' @param id,input,output,session Internal parameters for {shiny}.
+#' @param id,r,input,output,session Internal parameters for {shiny}.
 #'
 #' @noRd
 #'
